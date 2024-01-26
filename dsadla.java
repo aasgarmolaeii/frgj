3687c96b867d6decffc86f8d0f66da118ee479a8
aasgarmolaeii <138676052+aasgarmolaeii@users.noreply.github.com>
2024-01-26 19:03:03 +0100

    Update dsadla.java

--- dsadla.java
+++ dsadla.java
@@ -1,7 +1,7 @@
 class Vehicle {
   protected String brand = "Fodfr";        // Vehicle attribute
   public void honk() {                    // Vehicle method
-    System.out.println("Tuttoo");
+    System.out.println("Tutto");
   }
 }
 
