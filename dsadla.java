077312f3f835b2953ceaab15fd7cdb5d2e5e7ecf
aasgarmolaeii <138676052+aasgarmolaeii@users.noreply.github.com>
2024-01-27 16:37:09 +0100

    Update dsadla.java

--- dsadla.java
+++ dsadla.java
@@ -6,7 +6,7 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-  private String modelName = "Mustag";    // Car attribute
+  private String modelName = "Mustnnnnnnnnag";    // Car attribute
   public static void main(String[] args) {
 
     // Create a myCar object
