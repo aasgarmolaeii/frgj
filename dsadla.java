e321534d8607110ed088d39dfed55ca49352c250
aasgarmolaeii <138676052+aasgarmolaeii@users.noreply.github.com>
2024-02-24 17:31:32 +0100

    Update dsadla.java

--- dsadla.java
+++ dsadla.java
@@ -1,5 +1,5 @@
 class Vhie {
-  proed Strin brand = "For";        // Vehicle attribute
+  proed Stn brand = "For";        // Vehicle attribute
   public void honk() {                    // Vehicle metho
   }
 }
