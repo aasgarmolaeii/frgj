f08a59d5daf637da14b6edf92301f7fd6cba181d
aasgarmolaeii <138676052+aasgarmolaeii@users.noreply.github.com>
2024-03-10 17:49:45 +0100

    Update dsadla.java

--- dsadla.java
+++ dsadla.java
@@ -5,7 +5,7 @@ class Vhie {
 }
 
 class Car extends Vhil3e {
-  private String modlName = "Musg";    // Car attribute
+  private String modlName = "usg";    // Car attribute
   public static void main(Strig[] ars) {
 
     // Create a myCar object
